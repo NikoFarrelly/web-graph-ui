a10ad9a81f22a41e9c01af7c00a6b9da8e509bc4
NikoFarrelly <nickhfarrelly@gmail.com>
2026-05-04 11:47:23 +0100

    added export setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import babel from '@rolldown/plugin-babel';
-import react, {reactCompilerPreset} from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({presets: [reactCompilerPreset()]})],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'WebGraphUI',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `web-graph-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-force-graph-3d'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-force-graph-3d': 'ForceGraph3D',
+        },
+      },
+    },
+  },
 });
