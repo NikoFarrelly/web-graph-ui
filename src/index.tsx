e7d6c7e38662423894de59813aa71341a9b31444
NikoFarrelly <nickhfarrelly@gmail.com>
2026-05-06 17:53:40 +0100

    include css in export

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@
  * All exports from this pkg are within this file.
  */
 import {FarrellyGraph} from './components/FarrellyGraph';
-
+import './components/Graph.css';
+import './components/InfoPanel.css';
+import './components/Controls.css';
 export default FarrellyGraph;
