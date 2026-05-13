8504025ea323453e12ea40aa2e2e2fd8778aa5ee
NikoFarrelly <dev@thefarrelly.com>
2026-05-13 17:14:58 +0100

    improved useWindowSize hook

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 export type WindowSize = {
   width: number;
@@ -12,54 +12,25 @@ export function useWindowSize(
   const [windowSize, setWindowSize] = useState<WindowSize>();
   const {horizontal, vertical} = padding;
 
-  // TODO make this more reactive, potentially shift to using ref?
   useEffect(() => {
     function handleResize() {
-      if (!window) return;
+      const width = maxWindowSize?.width
+        ? Math.min(window.innerWidth, maxWindowSize.width)
+        : window.innerWidth;
+      const height = maxWindowSize?.height
+        ? Math.min(window.innerHeight, maxWindowSize.height)
+        : window.innerHeight;
 
-      const hasDefinedMax = !!maxWindowSize;
-
-      // initial set windowSize set
-      if (!hasDefinedMax && !windowSize) {
-        return setWindowSize({
-          width: window.innerWidth - horizontal,
-          height: window.innerHeight - vertical,
-        });
-      } else if (!windowSize && hasDefinedMax) {
-        const width =
-          window.innerWidth > maxWindowSize?.width ? maxWindowSize.width : window.innerWidth;
-        const height =
-          window.innerHeight > maxWindowSize?.height ? maxWindowSize.height : window.innerHeight;
-        return setWindowSize({
-          width: width - horizontal,
-          height: height - vertical,
-        });
-      }
-
-      if (maxWindowSize?.width && maxWindowSize?.height) {
-        const hasMaxWidth = maxWindowSize.width >= window.innerWidth;
-        const hasMaxHeight = maxWindowSize.height >= window.innerHeight;
-        // if outside max bounds, return early.
-        if (hasMaxHeight || hasMaxWidth) return;
-        setWindowSize({
-          width: window.innerWidth - horizontal,
-          height: window.innerHeight - vertical,
-        });
-      } else {
-        setWindowSize({
-          width: window.innerWidth - horizontal,
-          height: window.innerHeight - vertical,
-        });
-      }
+      setWindowSize({
+        width: width - horizontal,
+        height: height - vertical,
+      });
     }
 
-    // Set initial size
     handleResize();
-
     window.addEventListener('resize', handleResize);
-
     return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  }, [horizontal, maxWindowSize?.height, maxWindowSize?.width, vertical]);
 
   return windowSize;
 }
